97827ecbb6c66974a411ab5658e233b60e4830cc
joey m <34293717+joeyy-miller@users.noreply.github.com>
2023-03-31 23:58:03 -0700

    New Commands: Quick Delete, Quick Replace
    + Quickly append a line by entering the line to append after the key 'a#'.
    + Quickly delete a given line number, doesn't take two steps anymore. Try: d# where # is the line you want to delete. Supports up to 99 lines.

--- vij.py
+++ vij.py
@@ -10,11 +10,11 @@ BUILD = "03252023a"
 
 # Check if a file name is given as an argument
 if len(sys.argv) < 2:
-    print("------- the VIJ editor -------")
-    print("| error: no input.           |")
-    print("| try:                       |")
-    print("| python3 vij.py [filename]  |")
-    print("------------------------------")
+    print("-------  the VIJ editor  -------")
+    print("| error: no input.             |")
+    print("| try:                         |")
+    print("| python3 vij.py [filename]    |")
+    print("--------------------------------")
     sys.exit(1)
 
 # Open the file in read-write mode, or create a new file if it does not exist
@@ -29,6 +29,7 @@ lines = file.readlines()
 
 # Print the file content and the current line number
 def print_file():
+    print("------------- vij ----------------")
     print(f"Editing {filename}")
     for i, line in enumerate(lines):
         print(f"{i+1}: {line}", end="")
@@ -37,14 +38,14 @@ def print_file():
 def prompt():
     print("\nvij Commands:")
     print("a  - append a new line at the end")
-    print("i  - insert a new line before a given line number")
-    print("i# - insert a new line before # line number")
-    print("d  - delete a given line number")
-    print("r  - replace a given line number with a new line")
-    print("s  - save the file")
-    print("sq - save the file and exit")
+    print("i# - insert a new line before ")
+    print("d#  - delete a given line number")
+    print("r#  - replace a given line number with a new line")
+    print("s[q]  - save the file, q and quit")
     print("h  - about the vij editor")
     print("q  - quit without saving")
+    print(" # - optional line number. [q] - optional paramter")
+    print("------------/ vij /---------------")
     return input("\nEnter a command: ")
 
 # Loop until the user quits or saves
@@ -93,11 +94,33 @@ while True:
                 print("Invalid line number.")
         except ValueError:
             print("Invalid line number.")
+        
+    elif re.match("d+[0-9][0-9]?", command):
+        # Delete a given line
+        try:
+            # Strip off the letter of the command, leaving us with just the line # to delete.
+            line_to_delete = int(command[1:])
+            if 1 <= line_to_delete <= len(lines):
+                lines.pop(line_to_delete - 1)
+            else:
+                print("Invalid line number.")
+        except ValueError:
+            print("Invalid line number.")
     
     elif command == "r":
         # Replace a given line number with a new line
         try:
             line_num = int(input("Enter a line number to replace: "))
+            if 1 <= line_num <= len(lines):
+                new_line = input("Enter a new line: ")
+                lines[line_num - 1] = new_line + "\n"
+            else:
+                print("Invalid line number.")
+        except ValueError:
+            print("Invalid line number.")
+    elif re.match("r+[0-9][0-9]?", command):
+        try:
+            line_num = int(command[1:])
             if 1 <= line_num <= len(lines):
                 new_line = input("Enter a new line: ")
                 lines[line_num - 1] = new_line + "\n"
